0bf28d08f112599415b3b96eb1919824dc7d82be
greatness4life <39627831+greatness4life@users.noreply.github.com>
2023-03-17 16:19:13 +0100

    Update Profile.js
    Adjusted the list items

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -17,13 +17,13 @@ const Profile = () => {
   };
 
   return (
-    <Card className={classes.content}>
+   <Card className={classes.content}>
       <ProfileForm onSaveData={handleSave} />
-      {profiles.map((profile) => {
-        const { name, age, id } = profile;
-        return (
-          <ul key={id}>
-            <li className={classes.users}>
+      <ul>
+        {profiles.map((profile) => {
+          const { name, age, id } = profile;
+          return (
+            <li className={classes.users} key={id}>
               <div className={classes.items}>
                 <h3>Full Name:</h3>
                 {name}
@@ -33,9 +33,9 @@ const Profile = () => {
                 {age}years
               </div>
             </li>
-          </ul>
-        );
-      })}
+          );
+        })}
+      </ul>
     </Card>
   );
 };
